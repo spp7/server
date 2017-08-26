3469c5ba88153fc32c811f42e1739c00d3f97730
ppsari <poppymighty@gmail.com>
2017-08-26 15:31:37 +0700

    tes

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,3 +1,4 @@
+var qs = require('qs');
 require('mongoose')
 require('dotenv').config()
 const SHA256 = require("crypto-js/sha256")
@@ -8,58 +9,77 @@ const axios = require('axios')
 const User = require('../models/user')
 const userdata = require('../config/user')
 
-const generateToken = (req, res) => {
-  console.log('generate token')
+const generateToken = (req, res, callback) => {
   var headers = {
-    "Authorization": process.env.Business_OAuth_Credential,
+    "Authorization": "Basic MDBhMmNlY2YtNTdhOS00OTVkLWIzMzctMDUzNzk0ODFjZWEyOjkwZjg2NmYwLTBiYjEtNDE5Zi1iZmNjLWFiZDNjZTY1ZDBlMQ==",
     "Content-Type": "application/x-www-form-urlencoded"
   }
-  var data = {
+  var datas = {
     "grant_type":"client_credentials"
   }
-  axios.post('https://api.finhacks.id/api/oauth/token', data, headers)
-  .then((res) => res.send(res))
-  .catch(err => res.send(err))
+  axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
+  axios.defaults.headers.common['Authorization'] = 'Basic MDBhMmNlY2YtNTdhOS00OTVkLWIzMzctMDUzNzk0ODFjZWEyOjkwZjg2NmYwLTBiYjEtNDE5Zi1iZmNjLWFiZDNjZTY1ZDBlMQ=='
+  axios.post('https://api.finhacks.id/api/oauth/token', qs.stringify(datas), headers)
+  .then((res) => {
+    callback(req,res,res.data.access_token)
+  })
+  .catch(err => {res.send(err)})
 }
+
 const testGetUser = (req, res) => {
-  generateToken(req, res)
+
+  var headers = {
+    "Authorization": "Basic MDBhMmNlY2YtNTdhOS00OTVkLWIzMzctMDUzNzk0ODFjZWEyOjkwZjg2NmYwLTBiYjEtNDE5Zi1iZmNjLWFiZDNjZTY1ZDBlMQ==",
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+  var datas = {
+    "grant_type":"client_credentials"
+  }
+  axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
+  axios.defaults.headers.common['Authorization'] = 'Basic MDBhMmNlY2YtNTdhOS00OTVkLWIzMzctMDUzNzk0ODFjZWEyOjkwZjg2NmYwLTBiYjEtNDE5Zi1iZmNjLWFiZDNjZTY1ZDBlMQ=='
+  axios.post('https://api.finhacks.id/api/oauth/token', qs.stringify(datas), headers)
+  .then((res) => {
+    token = res.data.access_token
+
+    let apisecret = process.env.Business_API_Secret
+    let k = `/banking/v4/corporates/${process.env.Business_Corporate_ID}/accounts/${process.env.Business_Account_No_1}`
+    let j = req.method
+    let l = ""
+    l = `${SHA256(l)}`.toLowerCase()
+    l = l
+    let g = token
+    let h = new Date().toISOString().replace('Z','+07:00')
+    let stringtosign = `${j}:${k}:${g}:${l}:${h}`
+    stringtosign = `${crypto.HmacSHA1(apisecret,stringtosign)}`
+
+    // "Origin": "182.16.165.75:3001",
+    // "Content-Type": "application/json",
+    // let header = {
+    //   "Authorization": `Bearer ${g}`,
+    //   "X-BCA-Key": process.env.Business_API_Key,
+    //   "X-BCA-Timestamp": `${h}`,
+    //   "X-BCA-Signature": stringtosign,
+    // }
+
+    axios.defaults.headers.common = {
+      "Authorization": `Bearer ${g}`,
+      "Content-Type": "application/json",
+      "X-BCA-Key": process.env.Business_API_Key,
+      "X-BCA-Timestamp": `${h}`,
+      "X-BCA-Signature": stringtosign,
+    }
+
+    // console.log(header)
+    axios.get(`${process.env.API_URL}${k}`)
+    .then((result) => {console.log("ga error");res.send(result)})
+    .catch(err => {console.log("masuk error"); res.send(err)})
+
+  })
+  .catch(err => {res.send(err)})
 
 
-  //
-  //
-  // let apisecret = process.env.Business_API_Secret
-  // let k = `/banking/v4/corporates/${process.env.Business_Corporate_ID}/accounts/${process.env.Business_Account_No_1}`
-  // let j = req.method
-  // let l = ""
-  // l = `${SHA256(l)}`.toLowerCase()
-  // l = l
-  // let g = process.env.Business_OAuth_Credential //=>access token
-  // let h = new Date().toISOString() //=>2016-02-03T10:00:00.000+07:00
-  // let stringtosign = `${j}:${k}:${g}:${l}:${h}`
-  // stringtosign = `${crypto.HmacSHA1(apisecret,stringtosign)}`
-  //
-  // // "Origin": "182.16.165.75:3001",
-  // // "Content-Type": "application/json",
-  // // let header = {
-  // //   "Authorization": `Bearer ${g}`,
-  // //   "X-BCA-Key": process.env.Business_API_Key,
-  // //   "X-BCA-Timestamp": `${h}`,
-  // //   "X-BCA-Signature": stringtosign,
-  // // }
-  //
-  // axios.defaults.headers.common = {
-  //   "Authorization": `Bearer ${g}`,
-  //   "Content-Type": "application/json",
-  //   "X-BCA-Key": process.env.Business_API_Key,
-  //   "X-BCA-Timestamp": `${h}`,
-  //   "X-BCA-Signature": stringtosign,
-  // }
-  //
-  // // console.log(header)
-  // axios.get(`${process.env.API_URL}${k}`)
-  // .then((result) => {console.log("ga error");res.send(result)})
-  // .catch(err => {console.log("masuk error"); res.send(err)})
-  //
+
+
 }
 
 
