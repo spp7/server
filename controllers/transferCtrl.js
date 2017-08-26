c6facc88aab058521f8332296671749894ba1a79
ppsari <poppymighty@gmail.com>
2017-08-27 01:30:45 +0700

    fix model

--- controllers/transferCtrl.js
+++ controllers/transferCtrl.js
@@ -1,5 +1,88 @@
 require('mongoose')
 const Transfer = require('../models/transfer')
+const User = require('../models/user')
+var moment = require('moment-timezone')
+const DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSSZ'
+const TIMEZONE = 'Asia/Jakarta'
+
+
+const generateToken = (type, callback) => {
+  let auth = process.env.Business_OAuth_Credential
+
+  let headers = {
+    "Authorization": `Basic ${auth}`,
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+  let datas = {
+    "grant_type":"client_credentials"
+  }
+  axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
+  axios.defaults.headers.common['Authorization'] = `Basic ${auth}`
+  axios.post('https://api.finhacks.id/api/oauth/token', qs.stringify(datas), headers)
+  .then((result) => {
+    let token = result.data.access_token
+    callback(token)
+  })
+  .catch(err => {
+    console.log('error di token')
+    console.log(err)
+  })
+}
+
+const generateSignature = (userData, status ,callback) => {
+
+  generateToken(function(token) {
+    let a = 'POST'
+    let b,c,d,e,f
+    let i = ''
+
+    b = `/banking/corporates/transfers`
+    c = process.env.Business_Client_ID
+    d = process.env.Business_Client_Secret
+    e = process.env.Business_API_Key
+    f = process.env.Business_API_Secret
+    let g = token
+    let h  = moment.tz(new Date(), TIMEZONE).format(DATE_FORMAT)
+    let body = {
+      CorporateID:userData.businessBanking.CorporateID,
+      SourceAccountNumber:userData.businessBanking.AccountNumber,
+      TransactionID:new Date().toISOString()+'_'+userData.detail.line,
+      TransactionDate:h,
+      ReferenceID:"12345/PO/2016",
+      CurrencyCode:"IDR",
+      Amount:req.body.amount,
+      BeneficiaryAccountNumber:process.env.Business_Account_No_2,
+      Remark1:status
+    }
+    i = JSON.stringify(body).replace(/\s/g, '')
+    let j = a.toUpperCase()
+    let k = encodeURI(b)
+    let l = crypto.createHash('sha256').update(i).digest('hex').toLowerCase()
+    let m = h
+    let o = crypto.createHmac('sha256', f).update(a+':' + k + ':' + g + ':' + l + ':' + h).digest('hex')
+
+    let requestOptions = {
+      url: `https://api.finhacks.id${b}`,
+      method: a,
+      headers: {
+        'X-BCA-KEY': e,
+        'Content-Type': 'application/json',
+        'X-BCA-TIMESTAMP': m,
+        'Authorization': 'Bearer ' + g,
+        'X-BCA-SIGNATURE': o
+      },
+      body: JSON.stringify(body)
+    }
+
+    request(requestOptions, function (err, response, body) {
+      if (body !== null){
+        console.log(JSON.parse(body))
+      }
+      else console.log(err)
+
+    })
+  })
+}
 
 const createTransfer = (req, res) => {
   let now = new Date()
@@ -12,16 +95,34 @@ const createTransfer = (req, res) => {
     grow: [{amount: req.body.grow}],
     spend: [{amount: req.body.spend}],
     total: {
-      save: req.body.save,
-      give: req.body.give,
-      grow: req.body.grow,
-      spend: req.body.spend
+      save: total.save || 0 + parseInt(req.body.save),
+      give: total.give || 0 + parseInt(req.body.give),
+      grow: total.grow || 0 + parseInt(req.body.grow),
+      spend: total.spend || 0 + parseInt(req.body.spend)
     }
   }
 
   let transfer = new Transfer(transferData)
   transfer.save((err, transfer) => {
-    res.send(err ? {err: err} : transfer)
+    if (err) res.send({err:err})
+    else {
+      User.findOne({'detail.line': line}, (err,user)=>{
+        user.balance = user.balance || 0 - parseInt(req.body.save) - parseInt(req.body.spend) - parseInt(req.body.grow) - parseInt(req.body.give)
+        if (err) res.send({err: 'Line user not found'})
+        else
+          generateSignature(
+            user,
+            'Give',
+            generateSignature(user,'Grow',
+              function(){
+                //spend ke e wallet
+              }
+            )
+          )
+
+      })
+    }
+    // res.send(err ? {err: err} : transfer)
   })
 }
 
@@ -49,10 +150,10 @@ const updateTransfer = (req, res) => {
         if (err) res.send(err)
         else {
           //spend
-          axios.post(`${process.env.API_URL}/ewallet/topup`, topupData)
-          .then((err,result) => {
-            res.send(err? {err: err} : result)
-          })
+          // axios.post(`${process.env.API_URL}/ewallet/topup`, topupData)
+          // .then((err,result) => {
+          //   res.send(err? {err: err} : result)
+          // })
 
         }
       })
