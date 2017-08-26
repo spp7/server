db67aff1baf8abb2e793739fe6fa8117fcb75935
ppsari <poppymighty@gmail.com>
2017-08-27 01:19:33 +0700

    user

--- models/user.js
+++ models/user.js
@@ -7,31 +7,18 @@ let userSchema = new Schema({
     text: String,
     expirationDate: Date
   },
-  fire: {
-    Transfer: {
-      FirstName: String,
-      Address1: String,
-      City: String,
-      CountryID: String,
-      AccountNumber: String,
-      BankCodeType: String,
-      BankCodeValue: String,
-      BankCodeID: String
-    },
-    Authentication: {
-      CorporateID: String,
-      AccessCode: String,
-      BranchCode: String,
-      UserID: String,
-      LocalID: String
-    }
+  businessBanking : {
+    CorporateID: String,
+    AccountNumber: String
   },
   ewallet: {
     CustomerName: String,
     DateOfBirth: String,
     EmailAddress: String,
     CustomerNumber: String,
-    IDNumber: String
+    IDNumber: String,
+    CompanyCode: String,
+    PrimaryID: String
   },
   detail: {
     line: String,
